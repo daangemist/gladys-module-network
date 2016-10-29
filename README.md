c81e22f376a98e08aeb85305b53fec40593bd578
matueranet <git@daan.matuera.net>
2016-10-29 15:21:42 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+#Introduction
+
+This module allows you to monitor the status of network devices. Network items
+are checked using `ping`. They can be reached via IP or DNS.
---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function(sails) {
+
+    //var setup = require('./lib/install.js');
+    //var start = require('./lib/start.js');
+
+    return {
+        //install: install,
+        //start: start,
+    };
+};
