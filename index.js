f3db6511e9417c7a109e93707cfcf7fb394aa046
matueranet <git@daan.matuera.net>
2016-11-05 22:09:43 +0100

    Started on gladys device integration.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 module.exports = function(sails) {
 
-    var ping = require('./lib/ping.js');
+    var ping = require('./lib/ping.js'),
+      scan = require('./lib/scan.js');
+
+    gladys.on('ready', function() {
+      scan();
+    });
 
     return {
         ping: ping
